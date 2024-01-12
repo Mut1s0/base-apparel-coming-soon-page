8cb0e56bacd206f26172fa6ccd268dad5194463a
Allan Musyoka <musyokaallan96@gmail.com>
2024-01-12 19:40:21 +0300

    Added some changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,9 @@ function Hero() {
 
                 <div className="text-center md:text-left flex flex-col gap-5 lg:items-start lg:justify-center">
 
-                    <h1 className="text-[2.5rem] sm:text-[5rem] md:text-[3rem] lg:text-[5.5rem] font-semiBold leading-[60px] sm:leading-[90px] md:leading-[60px] lg:leading-[100px] uppercase text-darkGrayishRed tracking-[15px]"><span className="text-desaturatedRed font-light">we're</span> <br /> coming <br /> soon</h1>
+                    <h1 className="text-[2.5rem] sm:text-[5rem] md:text-[3rem] lg:text-[4.5rem] font-semiBold leading-[60px] sm:leading-[90px] md:leading-[60px] lg:leading-[90px] uppercase text-darkGrayishRed tracking-[15px]"><span className="text-desaturatedRed font-light">we're</span> <br /> coming <br /> soon</h1>
 
-                    <p className="text-desaturatedRed font-regular text-[1rem] sm:text-[1.5rem] md:text-[1.1rem] lg:text-[1.3rem]">Hello fellow shoppers! We're currently building <br className="block md:hidden"/> our new <br className="hidden md:block"/> fashion store. Add your email below to <br className="block md:hidden"/> stay up-to-date with <br className="hidden md:block"/> announcements and our <br className="block md:hidden"/> launch deals</p>
+                    <p className="text-desaturatedRed font-regular text-[1rem] sm:text-[1.2rem] md:text-[1.1rem] lg:text-[1.3rem]">Hello fellow shoppers! We're currently building <br className="block md:hidden"/> our new <br className="hidden md:block"/> fashion store. Add your email below to <br className="block md:hidden"/> stay up-to-date with <br className="hidden md:block"/> announcements and our <br className="block md:hidden"/> launch deals</p>
 
                 </div>
 
